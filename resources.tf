a4df367f19d09ab3c8d440da2cea394147982f9d
rodrigo-albuquerque <digofarias@gmail.com>
2019-10-03 10:27:42 +0100

    added azure subnets

--- resources.tf
+++ resources.tf
@@ -33,3 +33,26 @@ resource "aws_security_group" "subnetsecurity" {
     protocol = "tcp"
   }
 }
+
+resource "azurerm_resource_group" "az_network" {
+  location = "westeurope"
+  name = "rodresgroup"
+}
+
+resource "azurerm_virtual_network" "blue_virtual_network" {
+  address_space = ["10.0.0.0/16"]
+  location = "westeurope"
+  name = "rodvirtnetwork"
+  resource_group_name = "${azurerm_resource_group.az_network.name}"
+  dns_servers = ["10.0.0.4", "10.0.0.5"]
+  subnet {
+    name = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+}
