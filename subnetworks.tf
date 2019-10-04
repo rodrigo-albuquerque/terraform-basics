014319d20bcd640b5b2a5ba0a3bd961140a55d7c
rodrigo-albuquerque <digofarias@gmail.com>
2019-10-04 22:02:48 +0100

    concrete example of terraform variables

--- subnetworks.tf
+++ subnetworks.tf
@@ -1,6 +1,6 @@
 resource "google_compute_subnetwork" "rod-subnet" {
-  ip_cidr_range = "10.0.1.0/24"
-  name = "rodsubnet"
+  ip_cidr_range = "${var.gcp_ip_cidr_range}"
+  name = "${var.subnet_names["subnet1"]}"
   network = "${google_compute_network.our_development_network.self_link}"
   region = "europe-west1"
 }
--- variables.tf
+++ variables.tf
@@ -1,18 +1,28 @@
-variable "newvariable" {
+// Google Compute Platform Variables
+variable network_name {
+    default = "thenewnet"
     type = "string"
-    default = "thisvalue"
+    description = "Just the network name for the Google Compute Platform"
 }
 
-variable "maptype" {
+variable gcp_ip_cidr_range {
+    default = "10.0.0.0/16"
+    type = "string"
+    description = "IP CIDR Range for Google VPC"
+}
+
+variable aws_ip_cidr_range {
+    default = "10.0.1.0/24"
+    type = "string"
+    description = "IP CIDR Range for AWS VPC."
+}
+
+variable subnet_names {
     type = "map"
-    default = {
-        subnet1 = "subnet1"
-        subnet2 = "subnet2"
-        subnet3 = "subnet3"
-    }
-}
 
-variable "listtype" { 
-    type = "list"
-    default = ["item1", "item2"]
+    default = {
+        subnet1 = "subnetone"
+        subnet2 = "subnettwo"
+        subnet3 = "subnetthree"
+    }
 }
